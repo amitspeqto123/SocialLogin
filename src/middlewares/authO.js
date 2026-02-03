0dee68793af8e2e040181d58fee4a68ff0f63600
amitspeqto123 <amit.kumargupta@speqto.com>
2026-02-03 19:05:18 +0530

    auth o completed

---
+++ src/middlewares/authO.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { auth } from "express-oauth2-jwt-bearer";
+
+export const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
+  tokenSigningAlg: "RS256",
+});
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { deleteProductController, productCreateController } from "../controllers/product.controller.js";
-import { authenticateJWT} from "../middlewares/authMiddleware.js";
+//import { authenticateJWT} from "../middlewares/authMiddleware.js";
+import { checkJwt } from "../middlewares/authO.js";
 const router = express.Router();
 
-router.post("/create", authenticateJWT, productCreateController);
-router.delete("/:id", authenticateJWT, deleteProductController)
+router.post("/create", checkJwt, productCreateController);
+router.delete("/:id", checkJwt, deleteProductController)
 
 export default router;
